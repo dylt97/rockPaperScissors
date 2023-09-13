5466064a34f051e785a7d31e47819c106367145c
Dylan Thomas <dylanthomas441@gmail.com>
2023-09-12 21:03:08 -0400

    Polished my function for getting a random computer choice and added a function for comparing player and computer choices which then return a result for who wins the round.
    I was so concerned with getting the code perfect I completely forgot
    send regular commits. Note to self, commit often.

--- myScript.js
+++ myScript.js
@@ -1,12 +1,51 @@
+let computerChoice;
+let playerChoice;
+
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3) + 1;
-    let computerChoice;
     if (random === 1) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if (random === 2) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     } else {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
     return computerChoice;
+}
+
+let roundResult;
+
+function playRound() {
+    getComputerChoice();
+    playerChoice = prompt("Rock, Paper, or Scissors?", "");
+
+    let playerLowercase = playerChoice.toLowerCase();
+   
+    if (playerLowercase === computerChoice) {
+        roundResult = `It's a tie! You both chose ${playerLowercase}!`;
+        
+    } else if (playerLowercase === "rock" && computerChoice === "scissors") {
+        roundResult = "You win! Rock crushes scissors!";
+
+    } else if (playerLowercase === "rock" && computerChoice === "paper") {
+        roundResult = "You lose! Paper covers rock!";
+
+    } else if (playerLowercase === "paper" && computerChoice === "rock") {
+        roundResult = "You win! Paper covers rock!";
+
+    } else if (playerLowercase === "paper" && computerChoice === "scissors") {
+        roundResult = "You lose! Scissors cuts paper!";
+        
+    } else if (playerLowercase === "scissors" && computerChoice === "paper") {
+        roundResult = "You win! Scissors cuts paper!";
+
+    } else if (playerLowercase === "scissors" && computerChoice === "rock") {
+        roundResult = "You lose! Rock crushes scissors!";
+
+    } else {
+        roundResult = "Invalid Selection! Please select rock, paper, or scissors!";
+    }
+
+    return roundResult;
+
 }
